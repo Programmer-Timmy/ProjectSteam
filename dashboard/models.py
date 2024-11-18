b2f697be816487075e072e81c7f2f49e107aa653
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-11-18 22:34:05 +0100

    some example of showing data

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,42 @@
+import json
+
 from django.db import models
 
-# Create your models here.
+# new games model
+class Games(models.Model):
+    appid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    release_date = models.DateField()
+    english = models.BooleanField()
+    developer = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    required_age = models.IntegerField()
+    achievements = models.IntegerField()
+    positive_ratings = models.IntegerField()
+    negative_ratings = models.IntegerField()
+    median_playtime = models.IntegerField()
+    average_playtime = models.IntegerField()
+    owners = models.CharField(max_length=255)
+    price = models.FloatField()
+
+    def __str__(self):
+        return json.dumps({
+            'appid': self.appid,
+            'name': self.name,
+            'release_date': self.release_date.strftime('%Y-%m-%d'),
+            'english': self.english,
+            'developer': self.developer,
+            'publisher': self.publisher,
+            'required_age': self.required_age,
+            'achievements': self.achievements,
+            'positive_ratings': self.positive_ratings,
+            'negative_ratings': self.negative_ratings,
+            'median_playtime': self.median_playtime,
+            'average_playtime': self.average_playtime,
+            'owners': self.owners,
+            'price': str(self.price),
+        })
+
+    class Meta:
+        db_table = 'games'
+        managed = False
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,8 +3,15 @@ from http.client import responses
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from dashboard.models import Games
+
+
 # Create your views here.
 
 @login_required
 def index(request):
-    return render(request, 'dashboard/index.html', {'page_title': 'Dashboard'})
+    # get the top 10 avarage playtime of the games
+
+    top_10_games = Games.objects.order_by('-average_playtime')[:10]
+
+    return render(request, 'dashboard/index.html', {'page_title': 'Dashboard', 'top_10_games': top_10_games})
