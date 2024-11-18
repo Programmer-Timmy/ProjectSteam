d10c2f15fd80baffd88b00ec28a17407df377bf7
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-11-18 23:15:40 +0100

    added a connection between categories

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,10 +1,8 @@
-import json
-
 from django.db import models
 
 # new games model
 class Games(models.Model):
-    appid = models.IntegerField(primary_key=True)
+    appid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     release_date = models.DateField()
     english = models.BooleanField()
@@ -20,23 +18,33 @@ class Games(models.Model):
     price = models.FloatField()
 
     def __str__(self):
-        return json.dumps({
-            'appid': self.appid,
-            'name': self.name,
-            'release_date': self.release_date.strftime('%Y-%m-%d'),
-            'english': self.english,
-            'developer': self.developer,
-            'publisher': self.publisher,
-            'required_age': self.required_age,
-            'achievements': self.achievements,
-            'positive_ratings': self.positive_ratings,
-            'negative_ratings': self.negative_ratings,
-            'median_playtime': self.median_playtime,
-            'average_playtime': self.average_playtime,
-            'owners': self.owners,
-            'price': str(self.price),
-        })
+        return f"{self.name} ({self.developer}) - {self.average_playtime} hours average playtime"
 
     class Meta:
         db_table = 'games'
+        managed = False
+
+# game_categories model
+class GameCategories(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        db_table = 'game_categories'
+        managed = False
+
+# game_categories_link model
+class GameCategoriesLink(models.Model):
+    id = models.AutoField(primary_key=True)
+    app = models.ForeignKey(Games, on_delete=models.CASCADE, related_name='games', to_field='appid')
+    category = models.ForeignKey(GameCategories, on_delete=models.CASCADE, related_name='game_categories', to_field='category_id')
+
+    def __str__(self):
+        return f"{self.app.name} - {self.category.category_name}"
+
+    class Meta:
+        db_table = 'game_categories_link'
         managed = False
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,7 +3,7 @@ from http.client import responses
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from dashboard.models import Games
+from dashboard.models import Games, GameCategoriesLink
 
 
 # Create your views here.
@@ -14,4 +14,22 @@ def index(request):
 
     top_10_games = Games.objects.order_by('-average_playtime')[:10]
 
-    return render(request, 'dashboard/index.html', {'page_title': 'Dashboard', 'top_10_games': top_10_games})
+    games_with_categories = []
+    for game in top_10_games:
+        # Use select_related for optimized fetching of related category data
+        categories = GameCategoriesLink.objects.filter(app_id=game).select_related('category')
+        print(categories)
+        category_names = [category.category.category_name for category in categories]
+
+        games_with_categories.append({
+            'game': game,
+            'categories': category_names,
+        })
+
+    print(games_with_categories)
+
+    return render(request, 'dashboard/index.html', {
+        'page_title': 'Dashboard',
+        'top_10_games': top_10_games,
+        'games_with_categories': games_with_categories
+    })
